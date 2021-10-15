dfa75c2f9872761e465bc0274bb147c44bd46db5
Thomas Bewell <tombewell@gmail.com>
2021-10-15 17:55:48 -0500

    add resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,26 @@
 function Resume() {
+    return (
+        <section>
+            <h1>Resume</h1>
+            <a href="">Download my resume (coming soon)</a>
 
+            <h3>Proficiencies:</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>JavaScript</li>
+                <li>Third-Party APIs</li>
+                <li>Server-Side APIs</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>NoSQL</li>
+                <li>Progressive Web Applications</li>
+                <li>React</li>
+            </ul>
+        </section>
+    )
 }
 
 export default Resume;
