724cf7ebb088f8759ee74a7bcf77c431e49e99aa
Thomas Bewell <tombewell@gmail.com>
2021-10-15 20:24:42 -0500

    add footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,15 @@
 function Footer() {
     return (
-        <footer>
-
+        <footer className="flex-row px-1">
+            <a href="https://github.com/thomasbewell" target="_blank" rel="noreferrer">
+                GitHub
+            </a>
+            <a href="https://www.linkedin.com/in/thomas-bewell-279410223/" target="_blank" rel="noreferrer">
+                Linkedin
+            </a>
+            <a href="https://stackoverflow.com/users/15996478/thomas-bewell" target="_blank" rel="noreferrer">
+                Stack Overflow
+            </a>
         </footer>
     )
 }
